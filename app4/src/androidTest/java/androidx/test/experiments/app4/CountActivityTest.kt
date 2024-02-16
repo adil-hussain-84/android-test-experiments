1f35cda7103fc6c9ff2c87054356d2662fd2d6fa
Adil Hussain <adilson05uk@gmail.com>
2024-02-16 16:57:44 +0000

    Added a test in `CountActivityTest` and `CountViewTest` that demonstrates how to test state restoration when an activity is recreated.

--- app4/src/androidTest/java/androidx/test/experiments/app4/CountActivityTest.kt
+++ app4/src/androidTest/java/androidx/test/experiments/app4/CountActivityTest.kt
@@ -1,6 +1,7 @@
 package androidx.test.experiments.app4
 
 import androidx.compose.ui.test.assertTextEquals
+import androidx.compose.ui.test.junit4.StateRestorationTester
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
@@ -24,6 +25,18 @@ class CountActivityTest {
     fun tapping_the_increment_count_button_bumps_up_the_count_value_in_the_count_text() {
         // When.
         composeTestRule.onNodeWithTag("incrementCountButton").performClick()
+
+        // Then.
+        composeTestRule.onNodeWithTag("currentCountText").assertTextEquals("Count is 1")
+    }
+
+    @Test
+    fun count_text_is_restored_on_activity_recreation() {
+        // Given.
+        composeTestRule.onNodeWithTag("incrementCountButton").performClick()
+
+        // When.
+        activityScenarioRule.scenario.recreate()
 
         // Then.
         composeTestRule.onNodeWithTag("currentCountText").assertTextEquals("Count is 1")
--- app4/src/androidTest/java/androidx/test/experiments/app4/CountViewTest.kt
+++ app4/src/androidTest/java/androidx/test/experiments/app4/CountViewTest.kt
@@ -1,10 +1,10 @@
 package androidx.test.experiments.app4
 
 import androidx.compose.ui.test.assertTextEquals
+import androidx.compose.ui.test.junit4.StateRestorationTester
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -12,20 +12,38 @@ class CountViewTest {
 
     @get:Rule val composeTestRule = createComposeRule()
 
-    @Before
-    fun setUp() {
-        composeTestRule.setContent { CountView() }
-    }
-
     @Test
     fun count_text_before_the_increment_count_button_is_tapped() {
+        // When.
+        composeTestRule.setContent { CountView() }
+
+        // Then.
         composeTestRule.onNodeWithTag("currentCountText").assertTextEquals("Count is 0")
     }
 
     @Test
     fun tapping_the_increment_count_button_bumps_up_the_count_value_in_the_count_text() {
+        // Given.
+        composeTestRule.setContent { CountView() }
+
         // When.
         composeTestRule.onNodeWithTag("incrementCountButton").performClick()
+
+        // Then.
+        composeTestRule.onNodeWithTag("currentCountText").assertTextEquals("Count is 1")
+    }
+
+    @Test
+    fun count_text_is_restored_on_activity_recreation() {
+        // Given.
+        val stateRestorationTester = StateRestorationTester(composeTestRule)
+
+        stateRestorationTester.setContent { CountView() }
+
+        composeTestRule.onNodeWithTag("incrementCountButton").performClick()
+
+        // When.
+        stateRestorationTester.emulateSavedInstanceStateRestore()
 
         // Then.
         composeTestRule.onNodeWithTag("currentCountText").assertTextEquals("Count is 1")

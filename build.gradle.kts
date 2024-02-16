8e194600f59b9960f2217cf4c37da5fc68e9e166
Adil Hussain <adilson05uk@gmail.com>
2024-02-16 10:54:15 +0000

    Changed the "com.android.library" plugin in "build.gradle.kts" to ""com.android.application".

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 plugins {
-    id("com.android.library") version "8.2.2" apply false
+    id("com.android.application") version "8.2.2" apply false
     id("com.github.ben-manes.versions") version "0.51.0"
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
